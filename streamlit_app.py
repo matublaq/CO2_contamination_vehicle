f1406c5a51afd3023330907641f1726f0a7d896b
Matias Blaquier <matiasblaquier@Matiass-Laptop.local>
2024-07-13 12:41:13 +0100

    none

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,28 @@ import seaborn as sns
 st.set_option('deprecation.showPyplotGlobalUse', False) # Delete warning
 
 
+st.markdown("<p style='font-size: 10px;'>Los datos utilizados para este proyecto, son datos ooficial es de la Union Europea. <a href='https://www.eea.europa.eu/en/datahub/datahubitem-view/fa8b1229-3db6-495d-b18e-9c9b3267c02b'>Datos</a></p>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; text-decoration: underline; color: skyblue;'>Emisiones de CO2 vehiculos</h1>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("<p style='font-size: 20px;'>Los datos utilizados para este proyecto, son datos ooficial es de la Union Europea. <a href='https://www.eea.europa.eu/en/datahub/datahubitem-view/fa8b1229-3db6-495d-b18e-9c9b3267c02b'>Datos</a></p>", unsafe_allow_html=True)
-st.markdown("<br><br>", unsafe_allow_html=True)
 
-st.markdown("<p style='font-size: 30px;'>Cuanto contaminan los vehiculos? <br> Los vehiculos electricos no producen emisiones de CO2</p><br><br>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 30px;'>Cuanto contaminan los vehiculos?</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 12px;'>No tendremos en cuenta los vehiculos electricos o hibridos</p><br><br>", unsafe_allow_html=True)
 
+# Data
 df = pd.read_csv('df_fit.csv')
 
+#Cuales son las medias de peso, emisiones, tamano de motor y consumo de combustible
 gr1 = df[['Mass_(kg)', 'CO2_emission_(g/km)', 'Engine_size', 'Fuel_consumption_(l/100km)']]
-
 gr1.hist(figsize=(8, 8))
 st.pyplot(plt.show())
 
-
+#Como se relaciona el consumo del combustible con las emisiones
 plt.scatter(df['Fuel_consumption_(l/100km)'], df['CO2_emission_(g/km)'], color='blue')
 plt.xlabel('Fuel consumption')
 plt.ylabel('CO2 emission')
 st.pyplot(plt.show())
 
-
+#Como se relaciona el tamano del motor con las emisiones
 plt.scatter(df['Engine_size'], df['CO2_emission_(g/km)'], color='blue')
 plt.xlabel('Engine size')
 plt.ylabel('CO2 emission')
@@ -35,7 +36,7 @@ st.pyplot(plt.show())
 
 
 # Modelo de Regresion lineal
-st.markdown("<p style='font-size: 20px; text-align: center;'>Modelo de regresion lineal</p>", unsafe_allow_html=True)
+st.markdown("<br><br><p style='font-size: 20px; text-align: center; color: orange; text-decoration: underline'>Modelo de regresion lineal</p>", unsafe_allow_html=True)
 
 msk = np.random.rand(len(df)) < 0.8 #msk = mask. Lista de len(df) numeros aleatorios entre el 0 y 1. aproximadamente el 80% del conjunto (<0.8)
 train = df[msk]
