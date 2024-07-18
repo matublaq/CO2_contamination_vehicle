c925c280c0377e63748ac23aee32cfc680edce74
Matublaq <mati.blaquier@gmail.com>
2024-07-18 19:32:58 +0100

    none

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import seaborn as sns
 st.set_option('deprecation.showPyplotGlobalUse', False) # Eliminamos los warnings
 
 
+
 st.markdown("<p style='font-size: 10px;'>Los datos utilizados para este proyecto, son datos ooficial es de la Union Europea. <a href='https://www.eea.europa.eu/en/datahub/datahubitem-view/fa8b1229-3db6-495d-b18e-9c9b3267c02b'>Datos</a></p>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; text-decoration: underline; color: skyblue;'>Emisiones de CO2 vehiculos</h1>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
@@ -14,8 +15,8 @@ st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("- <p style='font-size: 20px; color: orange;'>Que factores influyen en la emision de CO2?</p>", unsafe_allow_html=True)
 st.markdown("- <p style='font-size: 20px; color: orange;'>Que factores influyen mas en el consumo de combustible?</p>", unsafe_allow_html=True)
 st.markdown("- <p style='font-size: 20px; color: orange;'>Se puede predecir el consumo de combustibles o las emisiones de CO2 basandose en otras caracteristicas del vehiculo?</p>", unsafe_allow_html=True)
-st.markdown("<p style='font-size: 12px;'>No tendremos en cuenta los vehiculos electricos o hibridos</p><br><br>", unsafe_allow_html=True)
-
+st.markdown("<p style='font-size: 12px;'>No tendremos en cuenta los vehiculos electricos o hibridos</p>", unsafe_allow_html=True)
+st.markdown('--- \n --- \n<br>', unsafe_allow_html=True)
 
 # Data
 df = pd.read_csv('df_fit.csv')
@@ -24,36 +25,49 @@ df.drop(columns=['ID'], inplace=True)
 #####################################################################
 #Información general
 
+st.markdown("<p style='font-size: 25px;'>Datos generales</p>", unsafe_allow_html=True)
 st.dataframe(df.describe())
 
 #Distribucion de las variables
 df.hist(bins=30, figsize=(15, 10))
 st.pyplot(plt.show())
+st.markdown('---')
 
 #Relacion entre las variables
-#sns.pairplot(df)
-#st.pyplot(plt.show())
+container = st.container()
+
+def generate_pairplot():
+    sns.pairplot(df)
+    st.pyplot(plt.show())
+st.write('Este grafico requiero de 2 minutos aproximadamente')
+if st.button('Relacion entre las variables'):
+    generate_pairplot()
+st.markdown('<br>', unsafe_allow_html=True)
 
 #####################################################################
-#Preprocesamiento
+#Preprocesamiento y modelo
 df_fit = df
 df_fit = pd.get_dummies(df_fit, columns=['Fuel_type'])
 df_fit.rename(columns={'Fuel_type_petrol': 'petrol', 'Fuel_type_diesel': 'diesel'})
-st.dataframe(df_fit)
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-numerical_cols = ['Mass_(kg)', 'Engine_size', 'Fuel_consumption_(l/100km)']
-df_fit[numerical_cols] = scaler.fit_transform(df_fit[numerical_cols])
-st.dataframe(df_fit)
+numerical_cols = ['Mass_(kg)', 'Engine_size', 'Fuel_consumption_(l/100km)', 'CO2_emission_(g/km)']
+df_scaled = df_fit[numerical_cols]
+df_scaled = pd.DataFrame(scaler.fit_transform(df_scaled), columns=numerical_cols) #fit = Calcula la media y la desviacion estandar. transform = Transforma para que tenga una media de 0 y una desviacion estandar de 1
 
 #####################################################################
-#Análisis y modelado
+#Análisis
+st.markdown("<p style='font-size: 20px;'>Podemos destacar que</p>", unsafe_allow_html=True)
+st.markdown("\n- Mayor consumo de combustible, mayor seran las emisiones de CO2. \n- Menor peso, menor va a ser el consumo.", unsafe_allow_html=True)
+st.markdown("", unsafe_allow_html=True)
 
-corr_matrix = df_fit.corr() #Relacion entre 2 variables. 1 = Ambas variables aumentan en una proporcion fija. -1 = Una crece y al otra crece en una proporcion fija. 0 = Ninguna correlacion 
+corr_matrix = df_fit[['Mass_(kg)', 'Engine_size', 'Fuel_consumption_(l/100km)', 'CO2_emission_(g/km)']].corr() #Relacion entre 2 variables. 1 = Ambas variables aumentan en una proporcion fija. -1 = Una crece y al otra crece en una proporcion fija. 0 = Ninguna correlacion 
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
 st.pyplot(plt.show())
 st.write(corr_matrix)
+
+
 
 #####################################################################
 #Modelo predictivo
