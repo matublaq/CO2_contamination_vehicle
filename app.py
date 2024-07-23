ff33f236f50d6f9217d13fc3997f9d0e88b0f2fb
Matublaq <mati.blaquier@gmail.com>
2024-07-23 20:04:56 +0100

    last commit?

--- app.py
+++ app.py
@@ -8,14 +8,22 @@ st.set_option('deprecation.showPyplotGlobalUse', False) # Eliminamos los warning
 
 
 
-st.markdown("<p style='font-size: 10px;'>Los datos utilizados para este proyecto, son datos ooficial es de la Union Europea. <a href='https://www.eea.europa.eu/en/datahub/datahubitem-view/fa8b1229-3db6-495d-b18e-9c9b3267c02b'>Datos</a></p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 10px;'>Los datos utilizados para este proyecto, son oficial de la Union Europea. <a href='https://www.eea.europa.eu/en/datahub/datahubitem-view/fa8b1229-3db6-495d-b18e-9c9b3267c02b'>Datos</a></p>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; text-decoration: underline; color: skyblue;'>Emisiones de CO2 vehiculos</h1>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 
-st.markdown("- <p style='font-size: 20px; color: orange;'>Que factores influyen en la emision de CO2?</p>", unsafe_allow_html=True)
-st.markdown("- <p style='font-size: 20px; color: orange;'>Que factores influyen mas en el consumo de combustible?</p>", unsafe_allow_html=True)
-st.markdown("- <p style='font-size: 20px; color: orange;'>Se puede predecir el consumo de combustibles o las emisiones de CO2 basandose en otras caracteristicas del vehiculo?</p>", unsafe_allow_html=True)
-st.markdown("<p style='font-size: 12px;'>No tendremos en cuenta los vehiculos electricos o hibridos</p>", unsafe_allow_html=True)
+url = ''
+
+
+#
+st.markdown("<p style='font-size: 17px; color: whitebone;'>&nbsp;&nbsp;&nbsp; El <strong>calentamiento global</strong> es un tema mundial. Los gases de efecto invernadero es una de las causas, y segun la union europea, la principal.</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 17px; color: whitebone;'>&nbsp;&nbsp;&nbsp; El <strong>CO2</strong>, dióxido de carbono, es el gas que emiten los vehiculos que utilizan combustible fosil.</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 12px; color: whitebone;'>La UE quiere reducir estas emisiones. Estos son los objetivos. <a href='https://www.europarl.europa.eu/topics/es/article/20180920STO14027/reducir-las-emisiones-de-los-automoviles-nuevos-objetivos-de-co2'>Datos UE</a></p>", unsafe_allow_html=True)
+
+st.markdown("<p style='font-size: 12px;'>No tendremos en cuenta los vehiculos electricos e hibridos</p>", unsafe_allow_html=True)
+st.markdown("- <p style='font-size: 25px; color: orange;'>buenas</p>", unsafe_allow_html=True)
+st.markdown("- <p style='font-size: 25px; color: orange;'>buenas</p>", unsafe_allow_html=True)
+st.markdown("- <p style='font-size: 25px; color: orange;'>buenas</p>", unsafe_allow_html=True)
 st.markdown('--- \n --- \n<br>', unsafe_allow_html=True)
 
 # Data
@@ -25,7 +33,7 @@ df.drop(columns=['ID'], inplace=True)
 #####################################################################
 #Información general
 
-st.markdown("<p style='font-size: 25px;'>Datos generales</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 25px; text-align: center;'>Informacion general de los datos</p>", unsafe_allow_html=True)
 st.dataframe(df.describe())
 
 #Distribucion de las variables
@@ -39,35 +47,40 @@ container = st.container()
 def generate_pairplot():
     sns.pairplot(df)
     st.pyplot(plt.show())
-st.write('Este grafico requiero de 2 minutos aproximadamente')
-if st.button('Relacion entre las variables'):
+st.markdown("<p style='font-size: 25px;'>Relacion entre las variables.</p> Este grafico requiere de 2 min para mostrarse", unsafe_allow_html=True)
+if st.button('Gráfico'):
     generate_pairplot()
-st.markdown('<br>', unsafe_allow_html=True)
+st.markdown('--- \n <br>', unsafe_allow_html=True)
 
 #####################################################################
-#Preprocesamiento y modelo
+#Análisis
+
 df_fit = df
 df_fit = pd.get_dummies(df_fit, columns=['Fuel_type'])
 df_fit.rename(columns={'Fuel_type_petrol': 'petrol', 'Fuel_type_diesel': 'diesel'})
+
+
+st.markdown("<p style='font-size: 25px; text-align: center;'>Mapa de calor</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 25px;'>Correlacion de Pearson</p><br> Relacion lineal entre dos variables", unsafe_allow_html=True)
+st.markdown("- 1: Correlacion positiva perfecta. Cuando una variable sube, la otra sube", unsafe_allow_html=True)
+st.markdown("- -1: Correlacion negativa perfecta. Cuando una variable sube, la otra baja", unsafe_allow_html=True)
+st.markdown("- 0: No hay correlacion. No hay alguna correlacion entre las variables", unsafe_allow_html=True)
+
+corr_matrix = df_fit[['Mass_(kg)', 'Engine_size', 'Fuel_consumption_(l/100km)', 'CO2_emission_(g/km)']].corr() #Relacion entre 2 variables. 1 = Ambas variables aumentan en una proporcion fija. -1 = Una crece y al otra crece en una proporcion fija. 0 = Ninguna correlacion 
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+st.pyplot(plt.show())
+st.write(corr_matrix)
+
+st.markdown("&nbsp;&nbsp;&nbsp; Bajar el consumo de combustible es lo primordial para bajar las emisiones de CO2. <br>&nbsp;&nbsp;&nbsp; La relacion es cercana a perfecta positiva", unsafe_allow_html=True)
+
+#####################################################################
+#Preprocesamiento y modelo
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 numerical_cols = ['Mass_(kg)', 'Engine_size', 'Fuel_consumption_(l/100km)', 'CO2_emission_(g/km)']
 df_scaled = df_fit[numerical_cols]
 df_scaled = pd.DataFrame(scaler.fit_transform(df_scaled), columns=numerical_cols) #fit = Calcula la media y la desviacion estandar. transform = Transforma para que tenga una media de 0 y una desviacion estandar de 1
-
-#####################################################################
-#Análisis
-st.markdown("<p style='font-size: 20px;'>Podemos destacar que</p>", unsafe_allow_html=True)
-st.markdown("\n- Mayor consumo de combustible, mayor seran las emisiones de CO2. \n- Menor peso, menor va a ser el consumo.", unsafe_allow_html=True)
-st.markdown("", unsafe_allow_html=True)
-
-corr_matrix = df_fit[['Mass_(kg)', 'Engine_size', 'Fuel_consumption_(l/100km)', 'CO2_emission_(g/km)']].corr() #Relacion entre 2 variables. 1 = Ambas variables aumentan en una proporcion fija. -1 = Una crece y al otra crece en una proporcion fija. 0 = Ninguna correlacion 
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
-st.pyplot(plt.show())
-st.write(corr_matrix)
-
-
 
 #####################################################################
 #Modelo predictivo
